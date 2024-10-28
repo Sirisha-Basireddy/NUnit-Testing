9e14aa650d5e0b6d43de77ce3ac6337659b25792
Sirisha-Basireddy <Sbasireddy6019@conestogac.on.ca>
2024-10-28 13:55:22 +0530

    Add NUnitTesting sln

---
+++ NUnitDemonstration/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿/* Sirisha Basireddy */
+
+using NUnit.Framework;
+
+namespace NUnitDemonstration
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+
+            // Fix: Correct the condition back from Additional arbitrary condition
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    } 
+
+}
